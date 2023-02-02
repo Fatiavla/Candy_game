700acfa9c8eea62566117958260d77fb84f330c5
Fatiavla <bioafc@mail.ru>
2023-02-02 19:55:31 +0300

    smth chnge

--- loader.py
+++ loader.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher
 # создали бота и передаем токен
 
-bot = Bot('5867826853:AAHUf9VT9ztBpN2a2FHLOgBC253QLwLqCVY')
+bot = Bot('You_token_here')
 dp = Dispatcher(bot) #Dispatcher отвечает за команды
